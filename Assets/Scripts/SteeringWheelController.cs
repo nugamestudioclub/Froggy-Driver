542dcffe3821487779955f8f669c9d4b472f0285
BBBrassil <56983102+BBBrassil@users.noreply.github.com>
2022-01-22 20:46:21 -0500

    Improved steering wheel control based on rotation of mouse cursor

--- Assets/Scripts/SteeringWheelController.cs
+++ Assets/Scripts/SteeringWheelController.cs
@@ -2,17 +2,25 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class SteeringWheelController : MonoBehaviour {
-	[SerializeField]
-	[Range(0, float.MaxValue)]
-	private float steeringSpeed = 100.0f;
+	private const int MAX_DEGREES = 360;
+
 	[SerializeField]
 	[Range(0, float.MaxValue)]
 	private float resetSpeed = 50.0f;
+
+	private float angle = 0.0f;
+
 	[SerializeField]
-	[Range(0, 180)]
-	private float maxAngle = 30.0f;
-	private bool isHeld = false;
-	private const int MAX_DEGREES = 360;
+	[Range(0, MAX_DEGREES / 2)]
+	private float maxAngle = 90.0f;
+
+	private bool mouseIsOver = false;
+
+	private bool mouseIsDown = false;
+
+	private static float AngleBetween(Vector2 p1, Vector2 p2) {
+		return Mathf.Rad2Deg * Mathf.Atan2(p2.y - p1.y, p2.x - p1.x);
+	}
 
 	private static float Sign(float angle) {
 		return angle < (MAX_DEGREES / 2) ? 1 : -1;
@@ -21,58 +29,78 @@ public class SteeringWheelController : MonoBehaviour {
 	private float ClampToMax(float angle) {
 		return Sign(angle) > 0
 			? Mathf.Min(angle, maxAngle)
-			: MAX_DEGREES - Mathf.Min(MAX_DEGREES - angle, maxAngle);
+			: Mathf.Max(angle, MAX_DEGREES - maxAngle);
 	}
 
 	private static float ClampToZero(float angle) {
 		const float EPSILON = 0.01f;
 
-		return (Sign(angle) > 0 ? angle : MAX_DEGREES - angle) > EPSILON
-			? angle
-			: 0;
+		return Abs(angle) > EPSILON ? angle : 0;
+	}
+
+	private static float Abs(float angle) {
+		return Sign(angle) > 0 ? angle : MAX_DEGREES - angle;
 	}
 
 	private static bool IsZero(float angle) {
-		Debug.Log($"angle {angle}");
-
-		return Mathf.Approximately(angle, 0.0f)
-			   || Mathf.Approximately(angle, MAX_DEGREES);
+		return Mathf.Approximately(Abs(angle), 0);
 	}
 
 	private void Rotate(float deltaZ) {
 		transform.Rotate(0.0f, 0.0f, deltaZ);
 	}
+	private float MouseAngle() {
+		var pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+		return AngleBetween(
+			new Vector2(pos.x, pos.y),
+			new Vector2(transform.position.x, transform.position.y)
+		);
+	}
 
 	private void Update() {
-		float z = transform.rotation.eulerAngles.z;
-
-		Debug.Log($"held {isHeld}");
-
-		if( !isHeld ) {
+		if( !(mouseIsOver && mouseIsDown) ) {
+			float z = transform.rotation.eulerAngles.z;
 			float deltaZ = Sign(z) * resetSpeed * Time.fixedDeltaTime;
 
 			z = ClampToZero(transform.rotation.eulerAngles.z - deltaZ);
 			deltaZ = z - transform.rotation.eulerAngles.z;
-
+			
 			Rotate(deltaZ);
-			isHeld = IsZero(transform.rotation.eulerAngles.z);
+			mouseIsDown = IsZero(transform.rotation.eulerAngles.z);
 		}
 	}
 
-	private void OnMouseDown() {
-		isHeld = true;
+	private void OnMouseOver() {
+		mouseIsOver = true;
 	}
 
-	private void OnMouseDrag() {
-		float deltaZ = Input.GetAxis("Mouse X") * steeringSpeed * Time.fixedDeltaTime;
-		float z = ClampToMax(transform.rotation.eulerAngles.z - deltaZ);
+	private void OnMouseExit() {
+		mouseIsOver = false;
+	}
 
-		deltaZ = z - transform.rotation.eulerAngles.z;
-
-		Rotate(deltaZ);
+	private void OnMouseDown() {
+		mouseIsDown = true;
+		angle = MouseAngle();
 	}
 
 	private void OnMouseUp() {
-		isHeld = false;
+		mouseIsDown = false;
 	}
+
+	private void OnMouseDrag() {
+
+		if( mouseIsOver && mouseIsDown ) {
+			float mouseAngle = MouseAngle();
+			float deltaZ = mouseAngle - angle;
+			float z = ClampToMax(transform.rotation.eulerAngles.z + deltaZ);
+
+			deltaZ = z - transform.rotation.eulerAngles.z;
+
+			Rotate(deltaZ);
+
+			angle = mouseAngle;
+		}
+	}
+
 }
