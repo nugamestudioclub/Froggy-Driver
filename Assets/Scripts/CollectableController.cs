31000db7f383bfbfa6ce598d816a2ce471b199f8
BBBrassil <56983102+BBBrassil@users.noreply.github.com>
2022-01-24 21:37:51 -0500

    Added optional destroy flag

--- Assets/Scripts/CollectableController.cs
+++ Assets/Scripts/CollectableController.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 [RequireComponent(typeof(Collider2D))]
 public class CollectableController : MonoBehaviour {
 	[SerializeField]
+	private bool destroyOnCollect;
+
+	[SerializeField]
 	private GameObject item;
 
 	void OnTriggerEnter2D(Collider2D collision) {
 		if( collision.tag == "Player" )
 			World.Instance.Give(item);
-		Destroy(gameObject);
+		if( destroyOnCollect )
+			Destroy(gameObject);
 	}
 }
