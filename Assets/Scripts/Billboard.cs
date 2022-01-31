2e21d27fe91809e6081bdff072e56a31e4156717
BBBrassil <56983102+BBBrassil@users.noreply.github.com>
2022-01-30 23:29:09 -0500

    Removed debugging from billboard script

--- Assets/Scripts/Billboard.cs
+++ Assets/Scripts/Billboard.cs
@@ -6,9 +6,6 @@ public class Billboard : MonoBehaviour {
 
 
 	void Start() {
-		Debug.Log($"{nameof(cam)} null ? {cam == null}");
-		Debug.Log($"layer {gameObject.layer}");
-		Debug.Log($"camera {World.Instance.Camera(gameObject.layer)}");
 		if( cam == null )
 			cam = World.Instance.Camera(gameObject.layer);
 	}
